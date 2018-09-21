f80c42d6a04d9be328f6a4abfa8842abdd15fcf0
goldenogs <goldenogs@gmail.com>
2018-09-20 21:40:16 -0700

    Wired up remove expensezs with firebase

--- src/test/action/expenses.test.js
+++ src/test/action/expenses.test.js
@@ -1,6 +1,6 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
+import { startAddExpense, addExpense, editExpense, removeExpense, startRemoveExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 import { start } from 'repl';
@@ -22,6 +22,22 @@ test('should setup remove expense action object', () => {
         id: '123abc'
     })
 });
+
+test('should remove expense from database', (done) => {
+  const store = createMockStore({});
+  const id = expenses[2].id;
+  store.dispatch(startRemoveExpense({id})).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'REMOVE_EXPENSE',
+      id
+    });
+    return database.ref(`expenses/${id}`).once('value');
+  }).then((snapshot) => {
+    expect(snapshot.val()).toBeFalsy();
+    done();
+  });
+})
 
 test('should update notes', () => {
     const action = editExpense('111222', {notes: 'hi im update'});
