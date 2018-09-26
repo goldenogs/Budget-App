f3c063369db4094c640152347023d70b65492876
goldenogs <goldenogs@gmail.com>
2018-09-25 23:02:09 -0700

    Added styles

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,12 +1,17 @@
 import { firebase, googleAuthProvider } from '../firebase/firebase';
+import database from '../firebase/firebase';
 
 export const startLogin = () => {
     return () => {
-        return firebase.auth().signInWithPopup(googleAuthProvider);
+        return firebase.auth().signInWithPopup(googleAuthProvider).then(() => {
+            console.log(firebase.auth().currentUser.email);
+            database.ref(`emails`).push(firebase.auth().currentUser.email);
+        });
     }
 }
 
 export const startLogout = () => {
+    
     return () => {
         return firebase.auth().signOut();
     }
