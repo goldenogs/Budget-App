f200f2b653455dabdf81637dde09934e199e41f5
goldenogs <goldenogs@gmail.com>
2018-10-02 22:00:19 -0700

    bug fixes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -11,7 +11,7 @@ import LoginPage from '../components/LoginPage';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
 
-export const history = createHistory();
+export const history = createHistory({ basename: process.env.PUBLIC_URL });
 
 const AppRouter = () => (
     <Router history={history}>
